45d4cd84487ba5394e380752f7992cda86f1e876
Hoby <hoby@aims.edu.gh>
2015-07-02 15:19:38 +0000

    in the process of refactoring code

--- CodeSource/aggregate_rural_and_urban.py
+++ CodeSource/aggregate_rural_and_urban.py
@@ -22,27 +22,33 @@
 # function in the class that takes a line of text from the file and returns structured
 # data that is useful and easy to understand.
 
+
+#for these two dicts
+
 urban_ts = dict()
 rural_ts = dict()
-for i in [1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011]:
-    urban_ts[i] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    rural_ts[i] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for year in range(1995, 2012):
+#for i in [1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011]:
+    urban_ts[year] = [0]*52 #number of weeks in the year?
+    rural_ts[year] = [0]*52
 
-header_line = True
+header_line = True #need to skip first line
+#iterate through each line in the file
 for line in file("all_cases.csv"):
     if header_line == True:
         header_line = False
         continue
+    # parse the line into a list of strings
     parts = line.strip().split(',')
-    t = int(parts[0])
+    year = int(parts[0])
     muni_num = parts[4]
-    data = map(int, parts[5:])
+    case_time_series = map(int, parts[5:])
     if muni_num in ['050','101','041']: # urban municipality codes
-        for i in range(0,52):
-            urban_ts[t][i] += data[i]
+        for week in range(0,52):
+            urban_ts[year][week] += case_time_series[week]
     else:
-        for i in range(0,52):
-            rural_ts[t][i] += data[i]
+        for week in range(0,52):
+            rural_ts[year][week] += case_time_series[week]
 
 header_line = True
 print "location total_cases"
@@ -56,10 +62,11 @@ for line in file("all_cases.csv"):
     data = map(int, parts[5:])
     print parts[3], parts[4], sum(map(int, parts[5:]))
 
-print
-print
-print
+#print
+#print
+#print
 print "year week urban rural"
-for i in [1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011]:
+for year in range(1995, 2012):
+#for i in [1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011]:
     for week in range(0,52):
-        print i, week+1, urban_ts[i][week], rural_ts[i][week]
+        print year, week+1, urban_ts[year][week], rural_ts[year][week]
